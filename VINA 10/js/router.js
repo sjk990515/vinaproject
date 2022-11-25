a4507ae7d65d54a6c20028299d8065cc4821407e
Minsung Kim <kim_minsung@@outlook.com>
2022-11-25 20:59:06 +0900

    router.js에서 주석 정리

--- VINA 10/js/router.js
+++ VINA 10/js/router.js
@@ -11,10 +11,8 @@ const routes = {
     404: "/pages/404.html",
     mainpage: "/pages/mainpage.html",
 };
-
 export const handleLocation = async () => {
-    let path = window.location.hash.replace("#", ""); // ""
-    // "http://example.com/"가 아니라 도메인 뒤에 / 없이 "http://example.com" 으로 나오는 경우
+    let path = window.location.hash.replace("#", "");
     if (path.length == 0) {
         path = "/";
     }
@@ -26,7 +24,6 @@ export const handleLocation = async () => {
     if (window.user) {
         document.getElementById("loginButton").style.display = "none";
         document.getElementById("logoutButton").style.display = "block";
-
         // 프로필 모달창 수정사항
         const openButton = document.getElementById("profile_BTN");
         const modal = document.querySelector(".modal");
@@ -34,27 +31,23 @@ export const handleLocation = async () => {
             modal.classList.remove("hidden");
             document.body.style.overflow = "hidden";
         });
-
         //모달 X버튼 닫기
         const modalExit = document.querySelector(".modalExit");
         modalExit.addEventListener("click", () => {
             modal.classList.add("hidden");
             document.body.style.overflow = "unset";
         });
-
         // 모달 cancel버튼 닫기
         const modalCancel = document.querySelector(".cancelBtn");
         modalCancel.addEventListener("click", () => {
             modal.classList.add("hidden");
             document.body.style.overflow = "unset";
         });
-
         // 게시물 페이지 모달
         const pageOpen = document.querySelector(".pageBTN");
         const pageOpen2 = document.querySelector(".pageBTN2");
         const pageModal = document.querySelector(".pageModal");
         const pageClose = document.querySelector(".pageExit");
-
         pageOpen2.addEventListener("click", () => {
             pageModal.classList.remove("hidden");
             document.body.style.overflow = "hidden";
@@ -67,7 +60,6 @@ export const handleLocation = async () => {
             pageModal.classList.add("hidden");
             document.body.style.overflow = "unset";
         });
-
         // 게시판 delete 버튼
         const pageDel = document.querySelector(".deleteBtn");
         const pageDesc = document.querySelector("#DESC");
@@ -93,48 +85,35 @@ export const handleLocation = async () => {
     } else {
         document.getElementById("loginButton").style.display = "block";
         document.getElementById("logoutButton").style.display = "none";
-
         /*alert (로그인전 경고)*/
         const openButton = document.getElementById("profile_BTN");
         const pageOpen = document.querySelector(".pageBTN");
         const pageOpen2 = document.querySelector(".pageBTN2");
-
         openButton.addEventListener("click", () => {
             if (confirm("로그인하세요")) {
                 window.location.hash = "#loginpage";
             }
         });
-
         pageOpen2.addEventListener("click", () => {
             if (confirm("로그인하세요")) {
                 window.location.hash = "#loginpage";
             }
         });
-
         pageOpen.addEventListener("click", () => {
             if (confirm("로그인하세요")) {
                 window.location.hash = "#loginpage";
             }
         });
-
         caches.delete(cacheName).then(() => { });
-        // 로그인이 안된 상태
     }
 
-    // 요소들 중에 login 버튼 < Header> 에 있는 것이 나타나고
-    // 이후에 로그인 버튼애ㅔ서 요소를 가져와서 inner.html
-    //로그인 했으면 로그아웃주고
     if (path === "mainpage") {
-        // 로그인한 회원의 프로필사진과 닉네임을 화면에 표시해줌.
         document.getElementById("profileNickname").placeholder =
             authService.currentUser.displayName ?? "닉네임 없음";
-
         document.getElementById("welcome_nickname").textContent =
             authService.currentUser.displayName + "님" ?? "닉네임 없음";
-
         document.getElementById("profileView").src =
             authService.currentUser.photoURL ?? "../assets/blankProfile.webp";
-
         getPostingList();
     }
 };
